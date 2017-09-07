d974da53abdd6ab25777e5032a772a13cf2899da
arpitbajaj <arpitbajaj95@gmail.com>
2017-09-07 15:14:16 +0530

    Removed seperate backroundworker

--- app/src/main/java/com/example/deepikasaini/imeijasoos/MainActivity.java
+++ app/src/main/java/com/example/deepikasaini/imeijasoos/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -34,6 +35,20 @@ import android.widget.Toast;
 import com.google.android.gms.common.api.CommonStatusCodes;
 import com.google.android.gms.vision.barcode.Barcode;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Scanner;
@@ -268,7 +283,159 @@ public class MainActivity extends AppCompatActivity implements  NavigationView.O
             insertIntoDB();
         }
     }
+    public class BackgroundWorker extends AsyncTask<String,String,String> {   //Activity,AsyncTask<String,String,String> {
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
 
 
+        Context context;
+        AlertDialog alertDialog;
+        BackgroundWorker(Context ctx){
+            context = ctx;
+        }
 
-}
+        TextView status;
+        TableLayout restable;
+
+
+        JSONObject object;
+
+        @Override
+        protected String doInBackground(String... params) {
+            String result = "";
+            String line;
+            String imei_status="";
+            String retval ="";
+            Log.d("debug",".............................BackgroundWorker");
+            String type = params[0];
+            String IMEInumber = params[1];
+            if(IMEInumber.length()!=15)
+            {
+                alertDialog = new AlertDialog.Builder(context).create();
+                alertDialog.setTitle("Please Enter a 15 digit IMEI");
+                alertDialog.show();
+                return "";
+
+            }
+
+            String login_url = "http://192.168.43.95/IMEIjasoos/valid.php";
+            if(type.equals("check")) try {
+                URL url = new URL(login_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                Log.d("debug", "....................................................connected");
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data = URLEncoder.encode("IMEInumber", "UTF-8") + "=" + URLEncoder.encode(IMEInumber, "UTF-8");
+//              URLEncoder.encode("username","UTF-8")+"="+URLEncoder.encode(username,"UTF-8")+"&"+URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                Log.d("debug", "...........................................result");
+                Log.d("debug", result);
+
+                try {
+                    object = new JSONObject(result);
+                    JSONArray jsonArray = object.getJSONArray("arr");
+                    JSONObject jsonObject = jsonArray.getJSONObject(0);
+                    imei_status = jsonObject.getString("Status");
+                    Log.d("debug","--------------JSON status");
+                    Log.d("debug",imei_status);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+                retval = imei_status;
+
+                if(!retval.equals("IMEI is invalid")){
+                    int sumval=0;
+                    String even_nu="";
+                    for(int i = 0; i<14;i++){
+                        //Log.d("mydebug", String.valueOf(imei_code.charAt(i)));
+                        if(i%2==0)
+                            sumval += Character.getNumericValue(IMEInumber.charAt(i));
+                        else {
+                            int temp = Character.getNumericValue(IMEInumber.charAt(i)) * 2;
+                            even_nu += temp;
+                        }
+                    }
+                    Log.d("debug",Integer.toString(sumval));
+                    Log.d("debug",even_nu);
+                    for(int i = 0; i<even_nu.length(); i++) {
+                        sumval += (Character.getNumericValue(even_nu.charAt(i)));
+                    }
+                    Log.d("debug",Integer.toString(sumval));
+                    int t2 = sumval%10;
+                    if(t2>0)
+                        t2=10-t2;
+                     if(t2==Character.getNumericValue(IMEInumber.charAt(14)))
+                        retval = "IMEI is Valid";
+                    else
+                        retval = "IMEI is Invalid";
+
+                }
+
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return retval;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return retval;
+
+        }
+
+
+        @Override
+        protected void onPreExecute() {
+            Log.d("debug",".............................onPreExecute");
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            Log.d("debug", ".............................onPostExecute");
+            Log.d("debug", result);
+
+            status = (TextView) ((Activity) context).findViewById(R.id.status);
+            String output = "", str1 = "", str2 = "";
+            String temp = "";
+            if (result.equals("IMEI is Valid")) {
+                try {
+                    JSONArray jsonArray = object.getJSONArray("arr");
+                    Log.d("debug", Integer.toString(jsonArray.length()));
+                    for (int i = 0; i < jsonArray.length(); i++) {
+                        temp = jsonArray.getJSONObject(i).toString();
+                        int idx = temp.indexOf(':');
+                        str1 = temp.substring(2, idx - 1);
+                        str2 = temp.substring(idx + 2, temp.length() - 2);
+                        output = output + "\n" + str1 + " : " + str2;
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                output = "Status : " + result;
+            }
+            status.setText(output);
+        }
+
+    }
+ }
+
